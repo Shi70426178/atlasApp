3fdfbf9b73851f63ba12565626a2d0e6c7dfbfd2
Kanhaiya Sharma <Kanhaiya@edatapings.com>
2026-06-02 12:55:58 +0530

    Added MAUI app and ASP.NET Core API

---
+++ AtlasAPI/Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using AtlasAPI.Data;
+using AtlasAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AtlasAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AuthController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("signup")]
+    public IActionResult Signup(User user)
+    {
+        var existingUser = _context.Users
+            .FirstOrDefault(x => x.Email == user.Email);
+
+        if (existingUser != null)
+        {
+            return BadRequest("Email already exists");
+        }
+
+        user.CreatedDate = DateTime.Now;
+
+        _context.Users.Add(user);
+        _context.SaveChanges();
+
+        return Ok("Signup successful");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(User loginUser)
+    {
+        var user = _context.Users.FirstOrDefault(x =>
+            x.Email == loginUser.Email &&
+            x.Password == loginUser.Password);
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid email or password");
+        }
+
+        return Ok("Login successful");
+    }
+}
